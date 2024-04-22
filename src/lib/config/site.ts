31ca216459ae3c0e6ab9dc25b6080e3a5b860a53
devbx <161192932+devbx@users.noreply.github.com>
2024-04-22 18:50:28 +0800

    Update domain

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'devbx.github.io',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'devbx.is-a.dev',
   title: 'DevBX',
   subtitle: 'My Blog',
   lang: 'en-US',
